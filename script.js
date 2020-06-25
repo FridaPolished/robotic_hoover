311e0d17ede7f40be544593afe64c42fa7ab7bd1
FridaPolished <fridapulido@gmail.com>
2020-06-25 14:56:22 -0700

    Adjust coordenates for elements in grid map

--- script.js
+++ script.js
@@ -17,17 +17,63 @@ function setData(){
   for (let i = 0; i < data.length; i++) {
     let cur = data[i];
     if(i === 0){
-      totalRows = +cur[0]
-      totalCols = +cur[1]; 
+      totalRows = +cur[2]
+      totalCols = +cur[0]; 
     } else if(i === 1){
-      hooverPos = [+cur[0], +cur[1]]
+      hooverPos = [+cur[2], +cur[0]]
+      // hooverPos = [0, 1]//test
     } else if( i === data.length - 1){
       directions = cur.split('');
     } else {
-     dirtPos.push([+cur[0], +cur[1]])
+     dirtPos.push([+cur[2], +cur[0]])
     } 
   }
 }
 
 
 
+//robot instructions
+function placeElements(){
+  let grid = new Array(totalRows).fill(null);
+  for (let i = 0; i < grid.length; i++) {
+    grid[i] = new Array(totalCols).fill('.')
+  }
+  
+  dirtPos = adjustPositions(dirtPos);
+  adjustPositions(hooverPos);
+  grid[hooverPos[0]][hooverPos[1]] = 'H'
+
+  dirtPos.forEach(dirt => {
+    let [row, col] = [...dirt]
+    grid[row][col] = 'D';
+  })
+  console.log(grid)
+
+}
+
+
+function adjustPositions(element){
+  
+  if(element.length > 2){
+    console.log(dirtPos, 'before update')
+    element = element.map(pos => {
+      let row = (totalRows - 1) - pos[0]
+      //let col = pos[1]
+      // console.log(row, pos)
+      pos[0] = row
+      return pos
+    })
+    // console.log(dirtPos, "dirt Positions updated")
+    return element;
+  } else {
+    let row = (totalRows - 1) - element[0]
+    let col = element[1]
+    // console.log(element, 'element')
+    // console.log(row, col, "'current target positions")
+     hooverPos[0] = row
+    //  hooverPos[1] = col;
+      return 
+  }
+}
+
+placeElements()
