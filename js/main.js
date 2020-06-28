5fae7eaf6dd09f628cb72addc87c7ac0cfb63625
FridaPolished <fridapulido@gmail.com>
2020-06-28 16:21:13 -0700

    Add missing path to get request

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ var input = document.getElementById('original-input');
 $(document).ready(function () {
   
   //reading data from txt
-  $.get("../input.txt", function (data) {
+  $.get("robotic_hoover/input.txt", function (data) {
     data = data.split("\n");
     Data.setData(data);
     Data.placeElements();
