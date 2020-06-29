c5ecfbee067aa3584e77619cb3bcfbb3df303848
FridaPolished <fridapulido@gmail.com>
2020-06-28 20:57:41 -0700

    Fix request

--- js/main.js
+++ js/main.js
@@ -12,16 +12,17 @@ opReq.send();
 
 function reqListener() {
   var data = this.responseText;
-  data = data.split("\n");
-      Data.setData(data);
-      Data.placeElements();
-      input.innerHTML = Data.showData({'input': true, 'output': false, 'res': null});
-      let res = Robot.run(
-        Data.totalRows, 
-        Data.totalCols, 
-        Data.hooverPos, 
-        Data.dirtPos, 
-        Data.directions,
-        Data.grid);
-      output.innerHTML = Data.showData({ 'input': false, 'output': true, 'res': res });
+  console.log(data)
+  // data = data.split("\n");
+  //     Data.setData(data);
+  //     Data.placeElements();
+  //     input.innerHTML = Data.showData({'input': true, 'output': false, 'res': null});
+  //     let res = Robot.run(
+  //       Data.totalRows, 
+  //       Data.totalCols, 
+  //       Data.hooverPos, 
+  //       Data.dirtPos, 
+  //       Data.directions,
+  //       Data.grid);
+  //     output.innerHTML = Data.showData({ 'input': false, 'output': true, 'res': res });
 }
