3226599c6db7dba4fe525544db7c2a3681feeddd
FridaPolished <fridapulido@gmail.com>
2020-06-28 21:39:59 -0700

    Undo previous edition

--- js/main.js
+++ js/main.js
@@ -4,27 +4,27 @@ import * as Robot from './robot.js';
 var output = document.getElementById('output');
 var input = document.getElementById('original-input');
 
+var oReq = new XMLHttpRequest();
+oReq.addEventListener("load", reqListener);
+oReq.open("GET", "../input.txt");
+// oReq.open("GET", "https://github.com/FridaPolished/robotic_hoover/blob/d372ec7e600e9d4b4532e7c8b09d745d0fdb6388/input.txt");
+oReq.send();
 
 
 var data;
 function reqListener() {
   data = this.responseText;
   console.log(data)
-  // data = data.split("\n");
-  //     Data.setData(data);
-  //     Data.placeElements();
-  //     input.innerHTML = Data.showData({'input': true, 'output': false, 'res': null});
-  //     let res = Robot.run(
-  //       Data.totalRows, 
-  //       Data.totalCols, 
-  //       Data.hooverPos, 
-  //       Data.dirtPos, 
-  //       Data.directions,
-  //       Data.grid);
-  //     output.innerHTML = Data.showData({ 'input': false, 'output': true, 'res': res });
+  data = data.split("\n");
+      Data.setData(data);
+      Data.placeElements();
+      input.innerHTML = Data.showData({'input': true, 'output': false, 'res': null});
+      let res = Robot.run(
+        Data.totalRows, 
+        Data.totalCols, 
+        Data.hooverPos, 
+        Data.dirtPos, 
+        Data.directions,
+        Data.grid);
+      output.innerHTML = Data.showData({ 'input': false, 'output': true, 'res': res });
 }
-var oReq = new XMLHttpRequest();
-oReq.addEventListener("load", reqListener);
-oReq.open("GET", "../input.txt");
-// oReq.open("GET", "https://github.com/FridaPolished/robotic_hoover/blob/d372ec7e600e9d4b4532e7c8b09d745d0fdb6388/input.txt");
-oReq.send();
