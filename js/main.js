7801308cd575baf05ce520d76820fc0361bf3ed2
FridaPolished <fridapulido@gmail.com>
2020-06-28 21:06:16 -0700

    Add new test

--- js/main.js
+++ js/main.js
@@ -9,19 +9,20 @@ opReq.addEventListener("load", reqListener);
 opReq.open("GET", "../input.txt");
 opReq.send();
 
+var data;
 function reqListener() {
-  var data = this.responseText;
+  data = this.responseText;
   console.log(data)
-  // data = data.split("\n");
-  //     Data.setData(data);
-  //     Data.placeElements();
-  //     input.innerHTML = Data.showData({'input': true, 'output': false, 'res': null});
-  //     let res = Robot.run(
-  //       Data.totalRows, 
-  //       Data.totalCols, 
-  //       Data.hooverPos, 
-  //       Data.dirtPos, 
-  //       Data.directions,
-  //       Data.grid);
-  //     output.innerHTML = Data.showData({ 'input': false, 'output': true, 'res': res });
+  data = data.split("\n");
+      Data.setData(data);
+      Data.placeElements();
+      input.innerHTML = Data.showData({'input': true, 'output': false, 'res': null});
+      let res = Robot.run(
+        Data.totalRows, 
+        Data.totalCols, 
+        Data.hooverPos, 
+        Data.dirtPos, 
+        Data.directions,
+        Data.grid);
+      output.innerHTML = Data.showData({ 'input': false, 'output': true, 'res': res });
 }
